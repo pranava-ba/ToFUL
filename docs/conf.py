5bbe19702e7e2e657fb28290d03d7a96b7e8dd9b
BA Pranava <97015345+pranava-ba@users.noreply.github.com>
2025-11-01 21:01:05 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# conf.py for ToFUL documentation
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'ToFUL'
+author = 'Pranava Ba'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Optional: autodoc settings
+autodoc_member_order = 'bysource'
