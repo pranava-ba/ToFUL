f4c82415a296dc5b5b9a778f84e459d066b2aa0b
BA Pranava <97015345+pranava-ba@users.noreply.github.com>
2025-11-03 12:07:58 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,46 @@
-project = 'ToFUL'
-author = 'Pranava Ba'
-release = '0.1.0'
+# Configuration file for the Sphinx documentation builder.
+# For the full list of built-in configuration values, see:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+project = 'ToFUL'
+copyright = '2024, Pranava BA'
+author = 'Pranava BA'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'myst_parser',  # if using Markdown
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+}
+
+# -- Napoleon settings -------------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+
+# -- Intersphinx mapping -----------------------------------------------------
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+}
